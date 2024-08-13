ead6203536e7697765c6b6438767657869384795
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-13 19:01:00 -0300

    Using Bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 // import App from './NormalCss_import/App.jsx'
 // import AppAll from './AppAll.jsx'
-import Sty from './Styled_Components/Sty'
+// import Sty from './Styled_Components/Sty'
+import BootsEx from "./Bootstrap/BootsEx";
 
-createRoot(document.getElementById('root')).render(
+// Bootstrap
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Sty />
-  </StrictMode>,
-)
+    <BootsEx />
+  </StrictMode>
+);
