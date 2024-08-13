53590b65166db4f21030adfb7a54d7a0c2ae0d25
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-13 18:24:23 -0300

    CSS Modules example

---
+++ src/AppAll.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Produto from './CSS_Modules/Produto'
+
+const AppAll = () => {
+  return (
+    <div>
+      <Produto/>
+    </div>
+  )
+}
+
+export default AppAll
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './NormalCss_import/App.jsx'
+// import App from './NormalCss_import/App.jsx'
+import AppAll from './AppAll.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppAll />
   </StrictMode>,
 )
