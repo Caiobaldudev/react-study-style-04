553b9c36ec8148698cbd6f9e93643a341d6f15d9
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-13 21:03:10 -0300

    Simple Animation Left

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import { createRoot } from "react-dom/client";
 // import App from './NormalCss_import/App.jsx'
 // import AppAll from './AppAll.jsx'
 // import Sty from './Styled_Components/Sty'
-import BootsEx from "./Bootstrap/BootsEx";
+// import BootsEx from "./Bootstrap/BootsEx";
+import AnimationApp from "./Animations/AnimationApp";
 
 // Bootstrap
 // import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BootsEx />
+    <AnimationApp />
   </StrictMode>
 );
